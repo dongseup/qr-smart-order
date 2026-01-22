2f96661fbceed765f24fca7efd7ddd22b99b28c6
dongseup lim <dongseup83@gmail.com>
2026-01-22 23:48:24 +0900

    서비스 제목 수정

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "QR Smart Order",
+  title: "QR 코드 기반 스마트 주문 시스템",
   description: "QR 코드 기반 스마트 주문 시스템",
 };
 
